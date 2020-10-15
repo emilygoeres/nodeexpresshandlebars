48c599830af961e87085ad30de603bcc225e8f77
Emily Goeres <goeres123@hotmail.com>
2020-10-15 16:37:55 -0600

    Added different burger flavors

--- Hamburger App/db/seeds.sql
+++ Hamburger App/db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO hamburgers (name) VALUES ('Cheese');
+INSERT INTO hamburgers (name) VALUES ('Bacon');
+INSERT INTO hamburgers (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO hamburgers (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO hamburgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO hamburgers (name) VALUES ('PB and J');

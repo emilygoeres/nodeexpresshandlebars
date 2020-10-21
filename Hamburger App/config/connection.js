fa11bdd977ac225435d72cfb73467b6df35f3748
Emily Goeres <goeres123@hotmail.com>
2020-10-20 19:28:57 -0600

    Added app to heroku

--- Hamburger App/config/connection.js
+++ Hamburger App/config/connection.js
@@ -1,6 +1,11 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+if(
+  process.env.JAWSDB_URL
+) {
+  var connection = mysql.createConnection (process.env.JAWSDB_URL)
+} 
+else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -8,7 +13,7 @@ var connection = mysql.createConnection({
   password: "password",
   database: "hamburger_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
